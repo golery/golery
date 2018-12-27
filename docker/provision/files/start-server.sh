7d1a2f940d17a09f919a0fe2eb30cffe1560a9e1
golery <noreply@mail.com>
2018-12-27 18:17:04 -0500

    Add log tee

--- docker/provision/files/start-server.sh
+++ docker/provision/files/start-server.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 #Start the server and run it for ever. If it crashes (after 5sec) then it will restarted automatically
-#https://github.com/foreverjs/forever
-#-m  MAX          Only run the specified script MAX times
-#-l  LOGFILE      Logs the forever output to LOGFILE
-#-o  OUTFILE      Logs stdout from child script to OUTFILE
-#-e  ERRFILE      Logs stderr from child script to ERRFILE
-#--minUptime      Minimum uptime (millis) for a script to not be considered "spinning" (ie. Fail to start)
-#--spinSleepTime  Time to wait (millis) between launches of a spinning script.
 source /softwares/golery/amazon.ecs.env.sh
 export NODE_ENV=production
 # delete all logs
