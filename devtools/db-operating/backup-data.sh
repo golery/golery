e68112a1ad4ad3bdb2f0641c5fbd6330405e66dc
golery <noreply@mail.com>
2018-12-29 18:46:33 -0500

    Add backup scripts

--- devtools/db-operating/backup-data.sh
+++ devtools/db-operating/backup-data.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
-#Copy data from EC2 /data to local git repostiory
-#EC2: /data
-#LOCAL: /work/www.data/data/
-#The local is stored in git lab repository
 #WARN ! For first use, on ec2. Run "sudo yum install rsync"
-. /work/www.credentials/env.sh
-rsync -av -e "ssh $AMAZON_IDENTITY" ec2-user@$AMAZON_HOST:/data/* /work/www.data/data/
+
+EC2_INSTANCE=18.224.97.10
+
+# COPY FROM AWS
+DATE=`date '+%Y%m%d-%H%M%S'`
+FOLDER="/data/awsbackup/$DATE"
+mkdir -p $FOLDER
+rsync -av -e "ssh -i /work/www.credentials/amazon/sshkeypair/id_rsa" ec2-user@$EC2_INSTANCE:/data/app-data $FOLDER
+cd $FOLDER/app-data
+
+# Create branch, commit and push
+BRANCH="backup-$DATE"
+git checkout -b $BRANCH
+git add --all && git commit -m "$BRANCH"
+git push --set-upstream origin $BRANCH
+
+echo Backup to $FOLDER, git commit and push to branch $BRANCH
