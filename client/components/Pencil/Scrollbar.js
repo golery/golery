d0515028a3712e9f007f6d72cfb343d4f3290c4f
golery <noreply@mail.com>
2019-02-02 21:15:19 -0500

    Smoothscroll

--- client/components/Pencil/Scrollbar.js
+++ client/components/Pencil/Scrollbar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
 import styles from './Scrollbar.scss';
-import ReactSmoothScrollbar from 'react-smooth-scrollbar';
 import SmoothScrollbar from 'smooth-scrollbar';
 import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
+import ReactSmoothScrollbar from 'react-smooth-scrollbar';
+
+SmoothScrollbar.use(OverscrollPlugin);
+
 export default class Scrollbar extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
+        let className = this.props.className || '';
+        className = [className, styles.scrollbar].join(' ');
         return (
-            <ReactSmoothScrollbar {...this.props}>
+            <ReactSmoothScrollbar {...this.props} className={className} plugins={{overscroll: { effect: 'glow'}}}>
                 {this.props.children}
             </ReactSmoothScrollbar>
         );
     }
 }
-
-Scrollbar.propTypes = {
-    //node: PropTypes.object
-};
