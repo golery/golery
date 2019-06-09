15b0b849a9d8057d530dfb4492ac3f66cfefd01b
golery <noreply@mail.com>
2019-06-08 22:31:46 -0400

    sitemap

--- devtools/build-prod.sh
+++ devtools/build-prod.sh
@@ -4,7 +4,7 @@ rm -rf build/release
 # Build client code
 ./node_modules/.bin/webpack --mode=production --progress --colors --config devtools/webpack/webpack.config.client.all.js
 # Build server code
-./node_modules/.bin/babel server -d build/release/server --copy-files
+./node_modules/.bin/babel server -d build/release/server --copy-files --extensions '.ts,.tsx,.js,.jsx' --source-maps
 # Install node modules
 cd /work/www.node-modules/www/prod
 ./npminstall.sh
